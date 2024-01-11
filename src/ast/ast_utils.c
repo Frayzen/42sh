fc4c19a199c4c2a9840bd7db012828b6d35d4909
florian.laine <florian@ynstant.io>
2024-01-11 18:26:30 +0100

    Format

--- src/ast/ast_utils.c
+++ src/ast/ast_utils.c
@@ -1,16 +1,14 @@
 #include "ast_utils.h"
+
 #include <stddef.h>
 #include <stdlib.h>
 
-
-struct ast *add_child(struct ast *src_ast,struct ast *new_ast)
+struct ast *add_child(struct ast *src_ast, struct ast *new_ast)
 {
-  if (!src_ast)
-    return new_ast;
-  src_ast->children = realloc(src_ast->children, nb_children + 1);
-  src_ast->children[nb_children] = new_ast;
-  src_ast->nb_children++;
-  return src_ast;
+    if (!src_ast)
+        return new_ast;
+    src_ast->children = realloc(src_ast->children, nb_children + 1);
+    src_ast->children[nb_children] = new_ast;
+    src_ast->nb_children++;
+    return src_ast;
 }
-
-
--- src/ast/ast_utils.h
+++ src/ast/ast_utils.h
@@ -3,13 +3,12 @@
 #include "ast.h"
 #include "token/token.h"
 
-
 /***
-* add_child: add a token to a tree
-* @param src_ast: the source tree to add the new ast to
-* @param new_ast: the tree to add to the source tree
-* @return the source tree with the new ast as child
-*/
-struct ast *add_child(struct ast* src_ast,struct ast *new_ast);
+ * add_child: add a token to a tree
+ * @param src_ast: the source tree to add the new ast to
+ * @param new_ast: the tree to add to the source tree
+ * @return the source tree with the new ast as child
+ */
+struct ast *add_child(struct ast *src_ast, struct ast *new_ast);
 
 #endif // AST_UTILS_H
