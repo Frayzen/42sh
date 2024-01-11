cce82a920a2e8a6cc776fed497ffdfca3e34b60e
tim pearson <timothy-edward.pearson@epita.fr>
2024-01-11 12:20:55 +0100

    added builtins, docstrings

--- src/token/token.c
+++ src/token/token.c
@@ -1,8 +1,17 @@
 #include "token.h"
 
-char *g_elements[9] = { [IF] = "if",      [THEN] = "then", [ELIF] = "elif",
-                        [ELSE] = "else",  [FI] = "fi",     [SEMI_COLON] = ";",
-                        [NEWLINE] = "\n", [QUOTE] = "'",   [WORD] = NULL };
+char *g_elements[NBTOKENS] = {
+    [IF] = "if",     [THEN] = "then",    [ELIF] = "elif",  [ELSE] = "else",
+    [FI] = "fi",     [SEMI_COLON] = ";", [NEWLINE] = "\n", [QUOTE] = "'",
+    [BSZERO] = "\0", [WORD] = NULL,      [TRUE] = "true",  [FALSE] = "false"
+};
+
+int is_terminating(struct token *token)
+{
+    if (!token)
+        return 0;
+    return token->type < 2;
+}
 
 int get_type(char *value)
 {
@@ -17,6 +26,7 @@ struct token *init_token(char *value)
     struct token *tok = malloc(sizeof(struct token));
     tok->type = get_type(value);
     tok->value = value;
+    tok->terminal = is_terminating(tok);
     return tok;
 }
 
@@ -35,4 +45,5 @@ void print_token(struct token *token)
         printf(" |NULL| ");
     else
         printf(" |%s|%d| ", token->value, token->type);
+    printf("\n");
 }
--- src/token/token.h
+++ src/token/token.h
@@ -1,29 +1,60 @@
 #ifndef TOKEN_H
 #define TOKEN_H
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
+#define NBTOKENS 13
 enum token_type
 {
+    NEWLINE = 0, // for is_terminating_char funciton
+    BSZERO = 1, // for is_terminating_char funciton
     IF,
     THEN,
     ELIF,
     ELSE,
     FI,
     SEMI_COLON,
-    NEWLINE,
     QUOTE,
     WORD,
-    TERMINAL
+    ECHO,
+    TRUE,
+    FALSE
 };
 
 struct token
 {
     enum token_type type;
     char *value;
+    bool terminal;
 };
+
+/***
+ * creates a token, sets value to lexime or 'term', sets type
+ * @param value the lexeme passed from the finder
+ * @return a malloced token structure
+ */
 struct token *init_token(char *value);
+
+/***
+ * frees a token
+ * @param tokenthe token to free
+ */
 void destroy_token(struct token *token);
+
+/***
+ * prints the token value and type number
+ * @param token token to print
+ */
 void print_token(struct token *token);
+
+/***
+ * checks if the token is a terminal character
+ * @param token the toekn to check
+ * @return 1 if the token is a terminal,
+ * 0 if null or non terminal
+ */
+int is_terminating(struct token *token);
 
 #endif // !TOKEN_H
