ef79a66c0a6123bccb02fdc44f1b945b7dd9ea61
tim pearson <timothy-edward.pearson@epita.fr>
2024-01-11 11:57:32 +0100

    added docstring, changed calloc

--- src/ast/ast.h
+++ src/ast/ast.h
@@ -9,7 +9,17 @@ struct ast
     int nb_children;
 };
 
+/***
+ * Create a ast node structure with no children
+ * @param token <- the token associated to the ast node 
+ * return <- malloced ast structure
+ */
 struct ast *init_ast(struct token *token);
+
+/***
+ * Recurisvely free an ast structure
+ * @param ast <- the ast to free
+ */
 void destroy_ast(struct ast *ast);
 
 #endif // !AST_H
