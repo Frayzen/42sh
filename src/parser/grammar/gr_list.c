c76b0d56b4aa18db7577a048af0003a6a3d3c442
salome.berger <salome.berger@epita.fr>
2024-01-12 17:56:48 +0100

    first shot list rule not tested

--- src/parser/grammar/gr_list.c
+++ src/parser/grammar/gr_list.c
@@ -1,6 +1,8 @@
 #include "lexer/token_saver.h"
 #include "rules.h"
 #include "tools/ast/ast.h"
+#include "tools/ast/ast_utils.h"
+#include "tools/token/token.h"
 
 enum status gr_line(struct ast **ast)
 {
@@ -24,5 +26,26 @@ enum status gr_line(struct ast **ast)
 
 enum status gr_list(struct ast **ast)
 {
-    return gr_and_or(ast); 
+    struct ast *ast_list = init_ast(AST_LIST, NULL);
+    enum status state = gr_and_or(&ast_list); 
+    if (state == ERROR)
+        return ERROR;
+    tok_pop();
+    while (state == OK)
+    {
+        add_child(*ast, ast_list);
+        if (tok_peek()->terminal)
+            break;
+        if (tok_peek()->type != SEMI_COLON)
+        {
+            state = ERROR;
+            break;
+        }
+        tok_pop();
+        state = gr_and_or(&ast_list);
+    }
+    if (tok_peek()->type == SEMI_COLON)
+        tok_pop();
+    *ast = add_child(*ast, ast_list);
+    return OK;
 }
