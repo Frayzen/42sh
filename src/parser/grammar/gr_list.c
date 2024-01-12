5860865421f4009f5b67f74f6b1849e4f8b8be8d
salome.berger <salome.berger@epita.fr>
2024-01-12 21:28:11 +0100

    removed gr_line

--- src/parser/grammar/gr_list.c
+++ src/parser/grammar/gr_list.c
@@ -6,26 +6,6 @@
 #include "tools/ast/ast_utils.h"
 #include "tools/token/token.h"
 
-enum status gr_line(struct ast **ast)
-{
-    enum status state = gr_and_or(ast);
-    if (state == ERROR)
-        return ERROR;
-    while (state == OK)
-    {
-        struct token *token = tok_peek();
-        if (token->type != SEMI_COLON)
-            return ERROR;
-        tok_pop();
-        state = gr_and_or(ast);
-    }
-    struct token *token = tok_peek();
-    if (!token || token->type == SEMI_COLON)
-        return OK;
-    // maybe need to check if there is something after, if yes -> ERROR ?
-    return ERROR;
-}
-
 enum status gr_list(struct ast **ast)
 {
     if (tok_peek()->terminal)
