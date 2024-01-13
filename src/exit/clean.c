51c3a086378fbcc1d066dbb851910bd65af103b3
florian.laine <florian@ynstant.io>
2024-01-13 14:48:12 +0100

    Created exit module

--- src/exit/clean.c
+++ src/exit/clean.c
@@ -1,10 +1,11 @@
 #include "exit.h"
 #include "io_backend/backend_saver.h"
 #include "tools/ast/ast.h"
+#include "lexer/token_saver.h"
 
 void clean(struct ast *ast)
 {
     destroy_ast(ast);
     clean_backend_saver();
-    clean_backend_saver();
+    clean_token_saver();
 }
