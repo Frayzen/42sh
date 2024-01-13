76e1027db72652d0e8199de2b884ef848fd6dc3c
florian.laine <florian@ynstant.io>
2024-01-13 15:15:35 +0100

    Working all tests

--- src/main.c
+++ src/main.c
@@ -8,16 +8,9 @@
 #include "tools/ast/ast.h"
 #include "tools/ast/ast_utils.h"
 
-/* int main(int argc, char *argv[]) */
-int main(void)
+int main(int argc, char *argv[])
 {
-    /* main_to_stream(argc, argv); */
-    char *argv[] = {
-        "fnieufn",
-        "-c",
-        "echo coucou uehouhf uehfi uhei uhfieu hifuhi i 'ehfieunf  biefue' e",
-    };
-    main_to_stream(3, argv);
+    main_to_stream(argc, argv);
     struct ast *ast = NULL;
     gr_input(&ast);
     exec_entry(ast);
