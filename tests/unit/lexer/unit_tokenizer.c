70d62db0c3bf593daf13881a645c1d8d7150b0b8
salome.berger <salome.berger@epita.fr>
2024-01-13 18:48:29 +0100

    finder str_maker fixed for backslash n token

--- tests/unit/lexer/unit_tokenizer.c
+++ tests/unit/lexer/unit_tokenizer.c
@@ -1,6 +1,7 @@
 #include <criterion/criterion.h>
 #include <criterion/internal/assert.h>
 #include <criterion/internal/test.h>
+#include <stdio.h>
 
 #include "exit/exit.h"
 #include "io_backend/backend_saver.h"
@@ -30,4 +31,19 @@ Test(tokenizer, test_if)
     cr_assert_eq(tok->terminal, false);
     destroy_token(tok);
     clean(NULL);
+}
+
+Test(tokenizer, test_bsn_fi)
+{
+    io_push("\nfi");
+    struct token *tok1 = next_token();
+    cr_assert_eq(tok1->type, NEWLINE);
+    cr_assert_str_eq(tok1->value, "\n");
+    cr_assert_eq(tok1->terminal, true);
+    destroy_token(tok1);
+    struct token *tok2 = next_token();
+    cr_assert_eq(tok2->type, FI);
+    cr_assert_str_eq(tok2->value, "fi");
+    cr_assert_eq(tok2->terminal, false);
+    destroy_token(tok2);
 }
