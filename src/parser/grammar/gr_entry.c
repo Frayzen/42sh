d33848d19daab04257478b935e7a9b69e08c36ca
salome.berger <salome.berger@epita.fr>
2024-01-13 13:59:19 +0100

    removed unecessary tok)pop and ast_destroy in gr_entry

--- src/parser/grammar/gr_entry.c
+++ src/parser/grammar/gr_entry.c
@@ -9,15 +9,9 @@ enum status gr_input(struct ast **ast)
 {
     enum status state = gr_list(ast);
     if (state == ERROR)
-    {
-        destroy_ast(*ast);
         goto error;
-    }
     if (!tok_peek()->terminal)
-    {
-        tok_pop();
         goto error;
-    }
     tok_pop();
     return OK;
 error:
