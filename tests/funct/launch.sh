9e79f4f2d686cf947f8863ba2f280031eb7da995
florian.laine <florian@ynstant.io>
2024-01-15 18:47:27 +0100

    Working color diff

--- tests/funct/launch.sh
+++ tests/funct/launch.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 cd $(dirname "$0")
 
-line_size=80
+line_size=90
 dash_line=$(printf '━%.0s' $(seq 1 $line_size))
 
 top_line=$(echo "┏$dash_line┓")
@@ -9,9 +9,22 @@ bot_line=$(echo "┗$dash_line┛")
 
 path_42sh=../../src/42sh
 
+#Black        0;30     Dark Gray     1;30
+#Red          0;31     Light Red     1;31
+#Green        0;32     Light Green   1;32
+#Brown/Orange 0;33     Yellow        1;33
+#Blue         0;34     Light Blue    1;34
+#Purple       0;35     Light Purple  1;35
+#Cyan         0;36     Light Cyan    1;36
+#Light Gray   0;37     White         1;37
+
 PASSED="\e[00;32mPASSED\e[0m"
 FAILED="\e[00;31mFAILED\e[0m"
 TIMEOUT="\e[00;31mTIMEOUT\e[0m"
+
+PURPLE="\e[00;35m"
+OTHER="\e[00;34m"
+RESET="\e[0m"
 
 file_id=0
 
@@ -31,19 +44,23 @@ print_line() {
     if [ $# -ge 2 ]; then
         to_add=$2
     fi
-    nb_char=$(echo $to_print | wc -m)
+    color="$3"
+    nb_char=$(echo "$to_print" | wc -m)
     nb_char=$(($line_size-$nb_char+$to_add+1))
-    echo -n "┃$to_print"
+    echo -n "┃"
+    printf '%b' "$color"
+    echo -n "$to_print"
     for _ in $(seq $nb_char); do
         echo -n " "
     done
+    printf '%b' "$RESET"
     echo "┃"
 }
 
 execute() {
-    modname=$2
+    modname=$(echo $2)
     id=$3
-    unique="$id\_$file_id"
+    unique="$(echo $id)_$file_id"
     theirs="../../theirs$unique"
     theirs_err="../../theirs_err$unique"
 
@@ -56,9 +73,9 @@ execute() {
     (timeout -k 0 1 $path_42sh $script) 1> $ours 2> $ours_err
     timeout=$?
     bash --posix $script 1> $theirs 2> $theirs_err
-    dif=$(diff $ours $theirs)
+    dif=$(diff -q $ours $theirs)
     res=$?
-    dif_err=$(diff $ours_err $theirs_err)
+    dif_err=$(diff -q $ours_err $theirs_err)
     res_err=$?
     toprint="($id)"
     toadd=12
@@ -80,7 +97,22 @@ execute() {
         toprint="$toprint$modname"
     fi
     print_line "$toprint" $toadd
-    rm $theirs $ours $ours_err $theirs_err $script
+    if [ $error -eq 1 ]; then
+        print_line "[ = REPORT = ]" 0 $PURPLE
+        if [ $res -ne 0 ]; then
+            print_line "= STDOUT differs" 0 $PURPLE
+            for difline in "$(diff -W 10 $ours $theirs))"
+            do
+                print_line "$(echo $difline | head -c $line_size)" 0 $OTHER
+            done
+            print_line "= END differs" 0 $PURPLE
+        fi
+        if [ $res_err -ne 0 ]; then
+            print_line "STDERR empty" 0 $PURPLE
+        fi
+        print_line "[ = END = ]" 0 $PURPLE
+    fi
+    # rm $theirs $ours $ours_err $theirs_err $script
 }
 
 echo $top_line
