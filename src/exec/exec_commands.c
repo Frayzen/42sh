c8108ce3540c1dc4d88b605b22e21e409e3b742f
florian.laine <florian@ynstant.io>
2024-01-16 17:31:25 +0100

    Added stdio checks

--- src/exec/exec_commands.c
+++ src/exec/exec_commands.c
@@ -90,27 +90,17 @@ int external_bin(struct ast *ast)
     if (pid == 0)
     {
         char **array_arg = create_command(ast);
-        int resp = execvp(array_arg[0], array_arg);
+        execvp(array_arg[0], array_arg);
         free(array_arg);
-        if (resp) // false
-        {
-            print_error(EXECVP_FAILED);
-            return 0;
-        }
-        else
-        {
-            int returncode;
-            waitpid(pid, &returncode, 0);
-            int code = 0;
-            if (WIFEXITED(returncode))
-                code = WEXITSTATUS(returncode);
-            if (code == 0)
-                return 0;
-            return 1;
-        }
+        exit(127);
     }
+    int returncode;
+    waitpid(pid, &returncode, 0);
+    int code = 1;
+    if (WIFEXITED(returncode))
+        code = WEXITSTATUS(returncode);
     fflush(stdout);
-    return 0;
+    return code;
 }
 
 int exec_external_bin(struct ast *ast)
---
+++ tests/funct/io/basics.sh
@@ -0,0 +1,8 @@
+echo ok;
+ls;
+pwd;
+#Hello you, why do you read that ?
+echo this; echo is; echo a; echo sentence;
+echo 'this is a quoted sentence';
+
+#Finally a comment
