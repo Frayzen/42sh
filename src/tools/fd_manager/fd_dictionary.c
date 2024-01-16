1fd2ecd8c2d6519945d32d06d7e973d864ad1fe8
salome.berger <salome.berger@epita.fr>
2024-01-16 23:04:28 +0100

    changed functions name

--- src/tools/fd_manager/fd_dictionary.c
+++ src/tools/fd_manager/fd_dictionary.c
@@ -3,14 +3,14 @@
 #include "exit/exit.h"
 #include "tools/fd_manager/fd_manager.h"
 
-struct fd_dictionary *init_fd_dictionary(void)
+struct fd_dictionary *dict_init(void)
 {
     struct fd_dictionary *fd_dico = malloc(sizeof(struct fd_dictionary));
     fd_dico->nb_entries = 0;
     return fd_dico;
 }
 
-struct fd_dictionary *get_fd_dictionary(struct fd_dictionary *dico)
+struct fd_dictionary *get_dict(struct fd_dictionary *dico)
 {
     static struct fd_dictionary *fd_dico = NULL;
     if (dico)
@@ -18,43 +18,43 @@ struct fd_dictionary *get_fd_dictionary(struct fd_dictionary *dico)
     return fd_dico;
 }
 
-void add_to_fd_dico(int old_fd, int new_fd)
+void dict_push(int old_fd, int new_fd)
 {
-    if (!FD_DICTIONARY)
-        get_fd_dictionary(init_fd_dictionary());
-    if (FD_DICTIONARY->nb_entries == DICTIONARY_SIZE)
+    if (!DICT)
+        get_dict(dict_init());
+    if (DICT->nb_entries == DICT_SIZE)
         exit_gracefully(FD_DICO_FULL);
-    FD_DICTIONARY->entries[FD_DICTIONARY->nb_entries].old_fd = old_fd;
-    FD_DICTIONARY->entries[FD_DICTIONARY->nb_entries].new_fd = new_fd;
-    FD_DICTIONARY->nb_entries++;
+    DICT->entries[DICT->nb_entries].old_fd = old_fd;
+    DICT->entries[DICT->nb_entries].new_fd = new_fd;
+    DICT->nb_entries++;
 }
 
-int get_new_fd(int old_fd)
+int dict_peek_new(int old_fd)
 {
-    for (int i = 0; i < FD_DICTIONARY->nb_entries; i++)
+    for (int i = 0; i < DICT->nb_entries; i++)
     {
-        if (FD_DICTIONARY->entries[i].old_fd == old_fd)
-            return FD_DICTIONARY->entries[i].new_fd;
+        if (DICT->entries[i].old_fd == old_fd)
+            return DICT->entries[i].new_fd;
     }
     return -1;
 }
 
-int get_old_fd(int new_fd)
+int dict_peek_old(int new_fd)
 {
-    for (int i = 0; i < FD_DICTIONARY->nb_entries; i++)
+    for (int i = 0; i < DICT->nb_entries; i++)
     {
-        if (FD_DICTIONARY->entries[i].new_fd == new_fd)
-            return FD_DICTIONARY->entries[i].old_fd;
+        if (DICT->entries[i].new_fd == new_fd)
+            return DICT->entries[i].old_fd;
     }
     return -1;
 }
 
-void remove_form_fd_dictionary(int old_fd)
+void dict_pop(int old_fd)
 {
     int index = -1;
-    for (int i = 0; i < FD_DICTIONARY->nb_entries; ++i)
+    for (int i = 0; i < DICT->nb_entries; ++i)
     {
-        if (FD_DICTIONARY->entries[i].old_fd == old_fd)
+        if (DICT->entries[i].old_fd == old_fd)
         {
             index = i;
             break;
@@ -62,17 +62,17 @@ void remove_form_fd_dictionary(int old_fd)
     }
     if (index == -1)
         return;
-    for (int i = index; i < FD_DICTIONARY->nb_entries - 1; ++i)
+    for (int i = index; i < DICT->nb_entries - 1; ++i)
     {
-        FD_DICTIONARY->entries[i] = FD_DICTIONARY->entries[i + 1];
+        DICT->entries[i] = DICT->entries[i + 1];
     }
-    FD_DICTIONARY->nb_entries--;
+    DICT->nb_entries--;
 }
 
-void free_fd_dictionary(void)
+void dict_free(void)
 {
-    if (!FD_DICTIONARY)
+    if (!DICT)
     {
-        free(FD_DICTIONARY->entries);
+        free(DICT->entries);
     }
 }
--- src/tools/fd_manager/fd_manager.h
+++ src/tools/fd_manager/fd_manager.h
@@ -1,8 +1,8 @@
 #ifndef FD_MANAGER_H
 #define FD_MANAGER_H
 
-#define DICTIONARY_SIZE 1024
-#define FD_DICTIONARY (get_fd_dictionary(NULL))
+#define DICT_SIZE 1024
+#define DICT (get_dict(NULL))
 
 struct fd_pair
 {
@@ -12,7 +12,7 @@ struct fd_pair
 
 struct fd_dictionary
 {
-    struct fd_pair entries[DICTIONARY_SIZE];
+    struct fd_pair entries[DICT_SIZE];
     int nb_entries;
 };
 
@@ -21,39 +21,39 @@ struct fd_dictionary
  * @param dico file descripor dictonary to set or NULL
  * @return the file descriptor dictonary
  */
-struct fd_dictionary *get_fd_dictionary(struct fd_dictionary *dico);
+struct fd_dictionary *get_dict(struct fd_dictionary *dico);
 
 /***
  * Adds an entry to the file descriptor dictionary
  * @param old_fd original file descriptor
  * @param new_fd new redirected file descriptor
  */
-void add_to_fd_dico(int old_fd, int new_fd);
+void dict_push(int old_fd, int new_fd);
 
 /***
  * Gives the new file descriptor associated with the file descriptor given
  * @param old_fd original file descriptor
  * @return the new redirected file descriptor
  */
-int get_new_fd(int old_fd);
+int dict_peek_new(int old_fd);
 
 /***
  * Gives the old file descriptor associated with the file descriptor given
  * @param new_fd new redirected file descriptor
  * @return the original file descriptor
  */
-int get_old_fd(int new_fd);
+int dict_peek_old(int new_fd);
 
 /***
  * Removes the entry associated with a file descriptor from the file descriptor
  * dictionary
  * @param old_fd original file descriptor to remove from the entries
  */
-void remove_form_fd_dictionary(int old_fd);
+void dict_pop(int old_fd);
 
 /***
  * Frees the file descriptor dictionary
  */
-void free_fd_dictionary(void);
+void dict_free(void);
 
 #endif /* !FD_MANAGER_H */
