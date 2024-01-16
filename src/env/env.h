693914840d81036d41a3d790048e3de4c21eb163
charlotte.marret@epita.fr <charlotte.marret@epita.fr>
2024-01-16 22:49:54 +0100

    env after comments

--- src/env/env.h
+++ src/env/env.h
@@ -4,39 +4,13 @@
 
 #include "tools/token/token.h"
 extern char **environ;
-
-/*
-union var_value
-{
-    int integer;
-    char char;
-    char *string;
-};
-
-
-enum var_type
-{
-    INT,
-    CHAR,
-    STRING,
-};
-
-struct env_var
-{
-    char *name;
-    enum var_type type;
-    union var_value value
-
-};
-*/
-
 // need to add the default value in the get_env_flag function for each new flag
+
 struct env
 {
     int print;
     int verbose;
     char **environ;
-    // struct env_var;
 };
 
 /***
