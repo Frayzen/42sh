8f8f753264321175521a26fbfb80feeb9e3336a3
florian.laine <florian@ynstant.io>
2024-01-20 19:54:33 +0100

    Working fine

---
+++ tests/funct/tests/and_or.sh
@@ -0,0 +1,14 @@
+### And basic
+echo ok && echo ok
+
+### And basic not
+! echo ok && echo ok
+
+### Or basic
+echo ok || echo ok
+
+### Or basic not
+! echo ok || echo ok
+
+### Long and
+echo ok && echo ok && ls && pwd && cat file && echo ok
