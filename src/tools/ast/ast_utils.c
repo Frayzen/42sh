13987409617d61279adc67806fb3d7093586c3d4
salome.berger <salome.berger@epita.fr>
2024-01-20 21:00:38 +0100

    missing and or ast

--- src/tools/ast/ast_utils.c
+++ src/tools/ast/ast_utils.c
@@ -1,3 +1,5 @@
+#include <assert.h>
+#include <stdbool.h>
 #include <stdlib.h>
 
 #include "parser/tools/gr_utils.h"
@@ -11,29 +13,16 @@
 #include "ast_utils.h"
 
 char *g_ast_types[] = {
-    [AST_CMD] = "CMD",       [AST_LIST] = "LST",  [AST_IF] = "IF",
-    [AST_PIPE] = "PIPE",     [AST_WHILE] = "WHL", [AST_UNTIL] = "UTL",
-    [AST_SH] = "SH",         [AST_ASS] = "ASS",   [AST_FOR] = "FOR",
-    [AST_AND_OR] = "AND_OR",
+    [AST_CMD] = "CMD",   [AST_LIST] = "LST",  [AST_IF] = "IF",
+    [AST_PIPE] = "PIPE", [AST_WHILE] = "WHL", [AST_UNTIL] = "UTL",
+    [AST_SH] = "SH",     [AST_ASS] = "ASS",   [AST_FOR] = "FOR",
 };
 
-int node_to_str(char *buf, struct ast *ast_root)
-{
-    if (!ast_root)
-    {
-        strcpy(buf, "[NULL]");
-        return 6;
-    }
-    int len = strlen(g_ast_types[ast_root->type]);
-    strcpy(buf, g_ast_types[ast_root->type]);
-    return len;
-}
+void pretty_print_ast_help(struct ast *ast, enum ast_type type, int depth,
+                           bool is_last_child, bool last_of_first);
 
-void pretty_print_ast_help(struct ast *ast_root, int depth, bool is_last_child,
-                           bool last_of_first)
+void align(int depth, bool is_last_child, bool last_of_first)
 {
-    if (!ast_root)
-        return;
     for (int i = 0; i < depth - 1; i++)
     {
         if (last_of_first)
@@ -48,10 +37,13 @@ void pretty_print_ast_help(struct ast *ast_root, int depth, bool is_last_child,
         else
             printf("╠══");
     }
-    static char buf[1024] = { 0 };
-    node_to_str(buf, ast_root);
-    printf("%s$\n", buf);
-    struct ast **children = get_children(ast_root);
+}
+
+void print_ast_list(struct ast_list *ast, int depth, bool is_last_child,
+                    bool last_of_first)
+{
+    printf("LST\n");
+    struct ast **children = get_children(AST(ast));
     if (!children)
         return;
     int i = 0;
@@ -66,20 +58,155 @@ void pretty_print_ast_help(struct ast *ast_root, int depth, bool is_last_child,
         }
         else
             is_last_child = false;
-        pretty_print_ast_help(child, depth + 1, is_last_child, last_of_first);
+        pretty_print_ast_help(child, AST(child)->type, depth + 1, is_last_child,
+                              last_of_first);
     }
-    free(children);
+}
+
+void print_ast_pipe(struct ast_pipe *ast, int depth, bool last_of_first)
+{
+    printf("PIPE\n");
+    depth++;
+    struct ast **children = get_children(AST(ast));
+    if (!children)
+        return;
+    int i = 0;
+    while (children[i])
+    {
+        struct ast *child = children[i++];
+        pretty_print_ast_help(child, child->type, depth + 1, false,
+                              last_of_first);
+    }
+    char *neg = ast->negated ? "neg = true" : "neg = false";
+    align(depth + 1, true, last_of_first);
+    printf("%s\n", neg);
+}
+
+void print_ast_if(struct ast_if *ast, int depth, bool last_of_first)
+{
+    printf("IF\n");
+    pretty_print_ast_help(AST(&ast->cond), AST_LIST, depth + 1, false,
+                          last_of_first);
+    pretty_print_ast_help(AST(&ast->then), AST_LIST, depth + 1, ast->fallback,
+                          last_of_first);
+    if (ast->fallback)
+    {
+        pretty_print_ast_help(ast->fallback, ast->fallback->type, depth + 1,
+                              true, last_of_first);
+    }
+}
+
+void print_ast_loop(struct ast_loop *ast, int depth, bool last_of_first)
+{
+    printf("LOOP\n");
+    pretty_print_ast_help(AST(&ast->cond), AST_LIST, depth + 1, false,
+                          last_of_first);
+    pretty_print_ast_help(AST(&ast->exec), AST_LIST, depth + 1, true,
+                          last_of_first);
+}
+
+void print_ast_cmd(struct ast_cmd *ast, int depth, bool last_of_first)
+{
+    printf("CMD\n");
+    depth++;
+    if (!ast->argc)
+        return;
+    align(depth, false, last_of_first);
+    printf("argv\n");
+    depth++;
+    align(depth, false, last_of_first);
+    printf("%s\n", ast->argv[0]);
+    for (int i = 1; i < ast->argc; i++)
+    {
+        if (i == ast->argc - 1)
+            align(depth, true, last_of_first);
+        else
+            align(depth, false, last_of_first);
+        printf("%s\n", ast->argv[i]);
+    }
+    depth--;
+    align(depth, true, last_of_first);
+    bool is_builtin = ast->type <= ECHO && ast->type >= T_TRUE;
+    printf("builtin %s\n", is_builtin ? "true" : "false");
+    return;
+}
+
+void print_ast_sh(struct ast_sh *ast, int depth, bool last_of_first)
+{
+    printf("SH\n");
+    align(depth, true, last_of_first);
+    pretty_print_ast_help(ast->sh_cmd, AST(ast->sh_cmd)->type, depth + 1, true, last_of_first);
+}
+
+void print_ast_for(struct ast_for *ast, int depth, bool last_of_first)
+{
+    printf("FOR\n");
+    align(depth, false, last_of_first);
+    if (!ast->item_list)
+    {
+        pretty_print_ast_help(AST(AST_LIST(ast)), AST_LIST, depth + 1, true, last_of_first);
+        return;
+    }
+    pretty_print_ast_help(AST(AST_LIST(ast)), AST_LIST, depth + 1, false, last_of_first);
+}
+
+void pretty_print_ast_help(struct ast *ast, enum ast_type type, int depth,
+                           bool is_last_child, bool last_of_first)
+{
+    align(depth, is_last_child, last_of_first);
+    switch (type)
+    {
+    case AST_LIST:
+        print_ast_list(AST_LIST(ast), depth, is_last_child, last_of_first);
+        break;
+    case AST_PIPE:
+        print_ast_pipe(AST_PIPE(ast), depth, last_of_first);
+        break;
+    case AST_IF:
+        print_ast_if(AST_IF(ast), depth, last_of_first);
+        break;
+    case AST_WHILE:
+    case AST_UNTIL:
+        print_ast_loop(AST_LOOP(ast), depth, last_of_first);
+        break;
+    case AST_CMD:
+        print_ast_cmd(AST_CMD(ast), depth, last_of_first);
+        break;
+    case AST_SH:
+        print_ast_sh(AST_SH(ast), depth, last_of_first);
+        break;
+    case AST_FOR:
+        print_ast_for(AST_FOR(ast), depth, last_of_first);
+        break;
+    default:
+        // Should not happend
+        // assert(false);
+        break;
+    }
 }
 
 void pretty_print_ast(struct ast *ast)
 {
     if (!ast)
     {
-        printf("\nNULL ast\n");
+        printf("\nNULL AST\n");
         return;
     }
-    pretty_print_ast_help(ast, 0, true, false);
+    pretty_print_ast_help(ast, AST(ast)->type, 0, true, false);
 }
+
+int node_to_str(char *buf, struct ast *ast_root)
+{
+    if (!ast_root)
+    {
+        strcpy(buf, "[NULL]");
+        return 6;
+    }
+    int len = strlen(g_ast_types[ast_root->type]);
+    strcpy(buf, g_ast_types[ast_root->type]);
+    return len;
+}
+
 #define START
 void ast_to_str_rec(struct ast *ast, char *buf, size_t *id)
 {
