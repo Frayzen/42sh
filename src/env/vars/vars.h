4b1079de1a4310508a935a8ee30e44adea892e83
tim pearson <timothy-edward.pearson@epita.fr>
2024-01-22 22:58:12 +0100

    assign and retrieve funcs

--- src/env/vars/vars.h
+++ src/env/vars/vars.h
@@ -1,8 +1,25 @@
 #ifndef VARS_H
 #define VARS_H
+#define _POSIX_C_SOURCE 200809L
+#include "env.h"
+#include <stdio.h>
+#define DBG_VAR(...)                                                          \
+    if (get_env_flag()->debug_pipe)                                            \
+        dprintf(DBG_OUT, __VA_ARGS__);
 
-void set_var(char *name, char *value);
+/*
+ * set the variable named name to the value
+ * @param name a string following the rules of the scl
+ * @param value that is already expanded
+ * return : the old value otherwise null
+ * */
+char *assign(char *name, char *value);
 
-char *retrieve_var(char *name);
+/*
+ * retrieved a value from the env variables
+ * @param name the name of the variable
+ * returns the string value of the variable otherwise null
+ */
+char *retrieve(char *name);
 
 #endif /* !VARS_H */
