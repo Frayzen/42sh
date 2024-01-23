c8c4793b359e544b89199a423a03eab017d44a7d
florian.laine <florian@ynstant.io>
2024-01-23 05:02:37 +0100

    Working - printing

--- src/exec/builtins/cd.c
+++ src/exec/builtins/cd.c
@@ -1,9 +1,12 @@
+#define _POSIX_C_SOURCE 200809L
+#include <linux/limits.h>
 #include <stddef.h>
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
 
 #include "builtins.h"
+#include "env/env.h"
 #include "env/vars/vars.h"
 
 void builtin_cd(char **argv)
@@ -14,12 +17,18 @@ void builtin_cd(char **argv)
     else
     {
         if (!strcmp(argv[1], "-"))
+        {
             new = retrieve_var("OLDPWD");
+            dprintf(STDOUT, "%s\n", new);
+        }
         else
             new = argv[1];
     }
     if (chdir(new) == -1)
         perror("Error on cd: ");
     else
-        assign_var("OLDPWD", assign_var("HOME", new));
+    {
+        char path[PATH_MAX];
+        assign_var("OLDPWD", assign_var("PWD", getcwd(path, PATH_MAX)));
+    }
 }
