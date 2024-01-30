3ed95f528f5c2577e2805b6ef1a1172c2e1a0f58
Charlotte Marret <charlotte.marret@epita.fr>
2024-01-30 21:44:33 +0100

    fix: pr comments

--- src/tools/funct_manager/funct_dict.c
+++ src/tools/funct_manager/funct_dict.c
@@ -69,30 +69,3 @@ void funct_dict_free(void)
     if (FUNCT_DICT)
         free(FUNCT_DICT->entries);
 }
-/*
-void funct_dict_print(void)
-{
-    if (!FUNCT_DICT->nb_entries)
-        return;
-    printf("┏━━━━━━━━━━━━━┓\n");
-    for (int i = 0; i < FUNCT_DICT->nb_entries; i++)
-    {
-        printf("┃ %d", FUNCT_DICT->entries[i].name);
-        if (FUNCT_DICT->entries[i].name < 1000)
-            printf(" ");
-        if (FUNCT_DICT->entries[i].name < 100)
-            printf(" ");
-        if (FUNCT_DICT->entries[i].name < 10)
-            printf(" ");
-        printf(" ↦ ");
-        if (FUNCT_DICT->entries[i].body < 1000)
-            printf(" ");
-        if (FUNCT_DICT->entries[i].body < 100)
-            printf(" ");
-        if (FUNCT_DICT->entries[i].body < 10)
-            printf(" ");
-        printf("%d ┃\n", FUNCT_DICT->entries[i].body);
-    }
-    printf("┗━━━━━━━━━━━━━┛\n");
-}
-*/
