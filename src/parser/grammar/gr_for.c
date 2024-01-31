dac6772a8ae41c1f56698d5046014da1f090fac0
Charlotte Marret <charlotte.marret@epita.fr>
2024-01-31 18:10:50 +0100

    fix: for

--- src/parser/grammar/gr_for.c
+++ src/parser/grammar/gr_for.c
@@ -19,10 +19,10 @@ enum status gr_for(struct ast_list *ast)
     tok_pop_clean();
     if (!IS_WORDABLE(tok_peek()))
         GR_DBG_RET(ERROR);
-    tok_pop_clean();
     struct ast_for *ast_for = init_ast(AST_FOR);
     ast_for->name = tok_peek()->str->value;
     tok_peek()->str->value = NULL;
+    tok_pop_clean();
     if (tok_peek()->type == SEMI_COLON)
         tok_pop_clean();
     else
