981d2fbbe40c1e4b0b037399daf8c75cc63d0837
tim pearson <timothy-edward.pearson@epita.fr>
2024-02-03 15:44:00 +0100

    more tests

--- tests/funct/tests/function.sh
+++ tests/funct/tests/function.sh
@@ -11,3 +11,15 @@ toto test
 ###Function all params
 toto () { echo $@ ; };
 toto this is a very long test
+
+###simple echo toto
+foo() { echo toto; }; foo
+
+###double function
+foo() { bar() { echo toto; } }; foo; bar
+
+###function with args easy
+foo() { echo $@; echo $1; echo $2; }; echo $@; foo toto tata
+
+###function with args medium
+foo() { echo $@; echo $1; echo $2; }; echo $@; foo toto tata; echo $@
