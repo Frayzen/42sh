4a0fc23e23d5fc42550e66fc7dfc1833e9a3511d
florian.laine <florian.laine@epita.fr>
2024-02-03 03:38:28 +0100

    feat: funcitons with params

--- tests/funct/tests/function.sh
+++ tests/funct/tests/function.sh
@@ -3,3 +3,11 @@ foo() { echo toto; }; foo
 
 ###double function
 foo() { bar() { echo toto; } }; foo; bar
+
+###Function params
+toto () { echo $1 ; };
+toto test
+
+###Function all params
+toto () { echo $@ ; };
+toto this is a very long test
