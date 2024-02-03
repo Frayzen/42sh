a85296521de2f3f0b1186bb49458d2a958ed9ff1
florian.laine <florian.laine@epita.fr>
2024-02-03 11:54:26 +0100

    fix: wip

--- tests/funct/tests/export.sh
+++ tests/funct/tests/export.sh
@@ -1,1 +1,12 @@
-#TODO add some test (using future $() expansion)
+### Export unkown
+export baab
+
+### Export a b c
+export a=3 b=4
+c=5
+echo $(echo $a $b $c)
+
+### Export bad name
+export first=tata @name=test other=toto
+echo $first $other
+echo $(echo $first $other)
